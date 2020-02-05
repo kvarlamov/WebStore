860a0a2625342363ae810adb5ca2dc7bf1f6ea15
kvarlamov <k.varlamov88@yandex.ru>
2020-02-05 12:48:39 +0300

    add to WebStoreContextInitializer Role and User

--- WebStore.Domain/Entities/Identity/User.cs
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -7,6 +7,8 @@ namespace WebStore.Domain.Entities.Identity
 {
     public class User : IdentityUser
     {
+        public const string Administrator = "Administrator";
+        public const string AdminPasswordDefault = "AdminPassword";
         public string Description { get; set; }
     }
 }
