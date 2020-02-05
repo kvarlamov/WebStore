84be96ac0635e5aa45731c1b035cd0da03b11f1c
kvarlamov <k.varlamov88@yandex.ru>
2020-02-05 18:00:14 +0300

    Cart implementation on cookies

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _CartService;
+
+        public CartController(ICartService CartService)
+        {
+            _CartService = CartService;
+        }
+
+        public IActionResult Details() => View(_CartService.TransformFromCart());
+
+        public IActionResult AddToCart(int id)
+        {
+            _CartService.AddToCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult DecrementFromCart(int id)
+        {
+            _CartService.DecrementFromCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            _CartService.RemoveFromCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult RemoveAll()
+        {
+            _CartService.RemoveAll();
+            return RedirectToAction("Details");
+        }
+    }
+}
