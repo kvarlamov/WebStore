d236bc5ce394f03d0caea0c84d7ba911d18b0e20
kvarlamov <k.varlamov88@yandex.ru>
2020-02-06 09:48:53 +0300

    Entities Order & OrderItem added to database + create migration Order

---
+++ WebStore.Domain/Entities/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities
+{
+    public class OrderItem:BaseEntity
+    {
+        public virtual Order Order { get; set; }
+        public virtual Product Product { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
