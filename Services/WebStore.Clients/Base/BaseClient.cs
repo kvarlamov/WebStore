1237ac81e7dc9da1e711fa1804366aad1199a955
kvarlamov <k.varlamov88@yandex.ru>
2021-05-13 23:36:26 +0300

    Final steps for base client

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebStore.Clients.Base
 {
@@ -24,5 +26,34 @@ namespace WebStore.Clients.Base
             headers.Clear();
             headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
+
+        private T Get<T>(string url) where T : new() => GetAsync<T>(url).Result;
+        
+        protected async Task<T> GetAsync<T>(string url, CancellationToken Cancel = default) where T: new()
+        {
+            var response = await _Client.GetAsync(url, Cancel);
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsAsync<T>(Cancel);
+
+            return new T();
+        }
+
+        private HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item, CancellationToken Cancel = default)
+        {
+            var response = await _Client.PostAsJsonAsync<T>(url, item, Cancel);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        private HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item, CancellationToken Cancel = default)
+        {
+            var response = await _Client.PutAsJsonAsync<T>(url, item, Cancel);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken Cancel = default) => await _Client.DeleteAsync(url, Cancel);
     }
 }
