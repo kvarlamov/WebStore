8cf487cf7b4c360ecb99d4f01525645193e7b71f
kvarlamov <k.varlamov88@yandex.ru>
2021-06-23 19:39:08 +0300

    Preparing controllers

---
+++ Common/WebStore.ServiceHosting/Controllers/RolesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleStore<IdentityRole> _RoleStore;
+        public RolesController(WebStoreContext db)
+        {
+            _RoleStore = new RoleStore<IdentityRole>(db) { AutoSaveChanges = true };
+        }
+    }
+}
---
+++ Common/WebStore.ServiceHosting/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserStore<User> _UserStore;
+        public UsersController(WebStoreContext db)
+        {
+            _UserStore = new UserStore<User>(db) { AutoSaveChanges = true };
+        }
+    }
+}
