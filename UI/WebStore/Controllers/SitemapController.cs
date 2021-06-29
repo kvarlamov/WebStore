1b1ac68215a41de2f60edf1276adf68101d27f0b
kvarlamov <k.varlamov88@yandex.ru>
2021-06-29 16:15:41 +0300

    Site Map

---
+++ UI/WebStore/Controllers/SitemapController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Controllers
+{
+    public class SitemapController : Controller
+    {
+        // GET
+        public IActionResult Index([FromServices] IProductData productData)
+        {
+            var nodes = new List<SitemapNode>
+            {
+                new SitemapNode(Url.Action("Index", "Home")),
+                new SitemapNode(Url.Action("ContactUs", "Home")),
+                new SitemapNode(Url.Action("Blog", "Home")),
+                new SitemapNode(Url.Action("BlogSingle", "Home")),
+                new SitemapNode(Url.Action("Shop", "Catalog")),
+                new SitemapNode(Url.Action("Index", "WebApiTest"))
+            };
+            
+            foreach (var section in productData.GetSections())
+            {
+                nodes.Add(new SitemapNode(Url.Action("Shop", "Catalog", new {SectionId = section.Id})));
+            }
+
+            foreach (var brand in productData.GetBrands())
+            {
+                nodes.Add(new SitemapNode(Url.Action("Shop", "Catalog", new {BrandId = brand.Id})));
+            }
+            
+            foreach (var product in productData.GetProducts(new ProductFilter()))
+            {
+                nodes.Add(new SitemapNode(Url.Action("Details", "Catalog", new {product.Id})));
+            }
+            
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}
