0229c49881fdd528963d332182747188e632260e
kvarlamov <k.varlamov88@yandex.ru>
2021-07-14 21:18:40 +0300

    Unobtrusive examples

---
+++ UI/WebStore/ViewModels/AjaxTestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebStore.ViewModels
+{
+    public class AjaxTestViewModel
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime ServerTime { get; set; } = DateTime.Now;
+    }
+}
